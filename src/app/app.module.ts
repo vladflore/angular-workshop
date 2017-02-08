dd7faf2ed24675885a38f2c02485300cae414357
Vlad Flore <flore.vlad@gmail.com>
2017-02-08 13:07:01 +0100

    Aufgaben_5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import {InfoBoxComponent} from "./info-box/info-box.component";
 import { MouseCursorComponent } from './mouse-cursor/mouse-cursor.component';
+import { TitleBoxComponent } from './title-box/title-box.component';
 
 @NgModule({
   declarations: [
-    AppComponent, InfoBoxComponent, MouseCursorComponent
+    AppComponent, InfoBoxComponent, MouseCursorComponent, TitleBoxComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/title-box/title-box.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { TitleBoxComponent } from './title-box.component';
+
+describe('TitleBoxComponent', () => {
+  let component: TitleBoxComponent;
+  let fixture: ComponentFixture<TitleBoxComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TitleBoxComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TitleBoxComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
