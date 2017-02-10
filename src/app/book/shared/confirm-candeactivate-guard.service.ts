9db807554ee3b19bcae8d745e6bd549eb4a7dd0f
Vlad Flore <flore.vlad@gmail.com>
2017-02-10 10:01:27 +0100

    Guard example

---
+++ src/app/book/shared/confirm-candeactivate-guard.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import {CanDeactivate, RouterStateSnapshot, ActivatedRoute, ActivatedRouteSnapshot} from '@angular/router';
+import {BookDetailComponent} from "../book-detail/book-detail.component";
+
+@Injectable()
+export class ConfirmCandeactivateGuardService implements CanDeactivate<BookDetailComponent>{
+
+  constructor() { }
+
+  canDeactivate(component: BookDetailComponent, route: ActivatedRouteSnapshot, snapshot:RouterStateSnapshot){
+    //params component: BookDetailComponent, route: ActivatedRouteSnapshot, snapshot:RouterStateSnapshot
+    return window.confirm("Are you sure?");
+  }
+
+}
